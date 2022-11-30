15c978a1515597ea8e45046be8b9db2619ee7cbf
Christian Deme <christian.deme@cleanercode.de>
2022-11-30 16:17:32 +0100

    Initial setup for app, SPA and specs

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  Rails.application.routes.draw do
+    root "spa#index"
+  end
+end
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+// eslint-disable-next-line no-undef, unicorn/prefer-module
+module.exports = {
+  content: [
+    './app/views/**/*.html.erb',
+    './app/helpers/**/*.rb',
+    './app/assets/stylesheets/**/*.css',
+    './app/javascript/**/*.ts',
+    './app/javascript/**/*.tsx',
+  ],
+};
