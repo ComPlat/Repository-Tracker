4e329f64cf981f413fed247fa45c331d6a44b2ff
Tobias Vetter <tobias.vetter@mailbox.org>
2022-12-05 19:55:49 +0100

    Adding some integration specs

--- spec/system/spa_spec.rb
+++ spec/system/spa_spec.rb
@@ -3,4 +3,27 @@ RSpec.describe "SPA" do
     visit "/"
     expect(page).to have_selector "h1", text: "Repository-Tracker"
   end
+
+  it do
+    visit "/"
+    expect(page).to have_text "ID"
+  end
+
+  it do
+    visit "/"
+    expect(page).to have_text "1"
+  end
+
+  it do
+    visit "/"
+    find(".ant-table-column-title", text: "ID").click
+    expect(page).to have_text "1"
+  end
+
+  it do
+    visit "/"
+    find(".ant-table-column-title", text: "ID").click.click
+
+    expect(page).to have_text "10000"
+  end
 end
