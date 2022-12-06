eb555fd82872ff0359d4299d5aaff842561ef734
Christian Deme <christian.deme@cleanercode.de>
2022-12-06 13:53:24 +0100

    Add authenticate method for User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,12 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
 
+  validates :email, format: URI::MailTo::EMAIL_REGEXP
   enum role: {user: 0, admin: 1}
+
+  # HINT: See: https://github.com/heartcombo/devise/wiki/How-To:-Find-a-user-when-you-have-their-credentials
+  def self.authenticate(email, password)
+    user = User.find_for_authentication(email: email)
+    user&.valid_password?(password) ? user : nil
+  end
 end
