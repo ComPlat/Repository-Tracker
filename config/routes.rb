a69b2292a5e55829eb84037d52e3af98a451148c
Christian Deme <christian.deme@cleanercode.de>
2022-12-06 13:51:00 +0100

    Change order of routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,11 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   Rails.application.routes.draw do
-    use_doorkeeper
     root "spa#index"
+
+    use_doorkeeper
+    devise_for :users
+
     mount API::Base => "/api"
     mount GrapeSwaggerRails::Engine => "/swagger"
   end
