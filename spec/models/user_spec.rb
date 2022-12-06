eebff2f810add6d55b9b0c6645baf3b15419a071
Tobias Vetter <tobias.vetter@mailbox.org>
2022-12-06 17:11:58 +0100

    Adding column specs for User

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,11 @@
 describe User do
-  subject(:user) { create(:user) }
+  subject(:user) { create :user }
+
+  describe "columns" do
+    it { is_expected.to have_db_column(:id).of_type(:integer) }
+    it { is_expected.to have_db_column(:created_at).of_type(:datetime) }
+    it { is_expected.to have_db_column(:updated_at).of_type(:datetime) }
+  end
 
   it { is_expected.to be_valid }
   it { is_expected.to have_many(:trackings) }
