f177844623d516332bc5330ba7b17232785a6bfe
Christian Deme <christian.deme@cleanercode.de>
2022-12-29 15:32:04 +0100

    Add routing specs for Devise and Doorkeeper

--- spec/routing/routes_spec.rb
+++ spec/routing/routes_spec.rb
@@ -1,5 +1,8 @@
 describe "Rails.application.routes.draw" do
   it { expect(get: "/").to route_to(controller: "spa", action: "index") }
   it { expect(get: "/swagger").to route_to(controller: "grape_swagger_rails/application", action: "index") }
+  it { expect(post: "/users").to route_to(controller: "users/registrations", action: "create") }
+  it { expect(post: "/oauth/token").to route_to(controller: "doorkeeper/tokens", action: "create") }
+  it { expect(post: "/oauth/revoke").to route_to(controller: "doorkeeper/tokens", action: "revoke") }
   it { expect(get: "/does_not_exists").not_to be_routable }
 end
