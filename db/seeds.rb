af0b20e25b0da42ec678b8f2e2f86a6c2615eba2
Christian Deme <christian.deme@cleanercode.de>
2023-01-10 12:19:04 +0100

    Add clear names for item ids in seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,46 +6,44 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-if Doorkeeper::Application.count.zero?
-  Doorkeeper::Application.create!(name: "React SPA API Client", redirect_uri: "")
-end
+Doorkeeper::Application.create!(name: "React SPA API Client", redirect_uri: "")
 
 # Example seeds for development and testing - REMOVE in production environment!
 
-User.create(email: "user@example.com",
+normal_user = User.create!(email: "user@example.com",
   password: "password",
   name: "User1",
   role: User.roles[:user])
-User.create(email: "admin@example.com",
+admin_user = User.create!(email: "admin@example.com",
   password: "password",
   name: "Admin1",
   role: User.roles[:admin])
-User.create(email: "super@example.com",
+super_user = User.create!(email: "super@example.com",
   password: "password",
   name: "Super1",
   role: User.roles[:super])
 
-TrackableSystem.create(name: :radar4kit)
-TrackableSystem.create(name: :radar4chem)
-TrackableSystem.create(name: :chemotion_repository)
-TrackableSystem.create(name: :chemotion_electronic_laboratory_notebook)
-TrackableSystem.create(name: :nmrxiv)
+ts_radar4kit = TrackableSystem.create!(name: :radar4kit)
+ts_radar4chem = TrackableSystem.create!(name: :radar4chem)
+ts_chemotion_repository = TrackableSystem.create!(name: :chemotion_repository)
+ts_chemotion_electronic_laboratory_notebook = TrackableSystem.create!(name: :chemotion_electronic_laboratory_notebook)
+ts_nmrxiv = TrackableSystem.create!(name: :nmrxiv)
 
-TrackingItem.create(name: "Tracking Item 1", user_id: 1)
-TrackingItem.create(name: "Tracking Item 2", user_id: 2)
-TrackingItem.create(name: "Tracking Item 3", user_id: 1)
-TrackingItem.create(name: "Tracking Item 4", user_id: 3)
-TrackingItem.create(name: "Tracking Item 5", user_id: 1)
+tracking_item1 = TrackingItem.create!(name: "Tracking Item 1", user_id: normal_user.id)
+tracking_item2 = TrackingItem.create!(name: "Tracking Item 2", user_id: admin_user.id)
+tracking_item3 = TrackingItem.create!(name: "Tracking Item 3", user_id: normal_user.id)
+tracking_item4 = TrackingItem.create!(name: "Tracking Item 4", user_id: super_user.id)
+tracking_item5 = TrackingItem.create!(name: "Tracking Item 5", user_id: normal_user.id)
 
-Tracking.create(status: :draft, metadata: {data: "some new data"}, tracking_item_id: 1,
-  from_trackable_system_id: 1, to_trackable_system_id: 2)
-Tracking.create(status: :pending, metadata: {data: "some more new data"}, tracking_item_id: 2,
-  from_trackable_system_id: 4, to_trackable_system_id: 3)
-Tracking.create(status: :submitted, metadata: {data: "some other new data"}, tracking_item_id: 5,
-  from_trackable_system_id: 4, to_trackable_system_id: 2)
-Tracking.create(status: :draft, metadata: {data: "some extraordinary data"}, tracking_item_id: 3,
-  from_trackable_system_id: 3, to_trackable_system_id: 1)
-Tracking.create(status: :pending, metadata: {data: "some great data"}, tracking_item_id: 2,
-  from_trackable_system_id: 4, to_trackable_system_id: 1)
-Tracking.create(status: :submitted, metadata: {data: "some awesome data"}, tracking_item_id: 4,
-  from_trackable_system_id: 5, to_trackable_system_id: 2)
+Tracking.create!(status: :draft, metadata: {data: "some new data"}, tracking_item_id: tracking_item1.id,
+  from_trackable_system_id: ts_radar4kit.id, to_trackable_system_id: ts_radar4chem.id)
+Tracking.create!(status: :pending, metadata: {data: "some more new data"}, tracking_item_id: tracking_item2.id,
+  from_trackable_system_id: ts_chemotion_electronic_laboratory_notebook.id, to_trackable_system_id: ts_chemotion_repository.id)
+Tracking.create!(status: :submitted, metadata: {data: "some other new data"}, tracking_item_id: tracking_item5.id,
+  from_trackable_system_id: ts_chemotion_electronic_laboratory_notebook.id, to_trackable_system_id: ts_radar4chem.id)
+Tracking.create!(status: :draft, metadata: {data: "some extraordinary data"}, tracking_item_id: tracking_item3.id,
+  from_trackable_system_id: ts_chemotion_repository.id, to_trackable_system_id: ts_radar4kit.id)
+Tracking.create!(status: :pending, metadata: {data: "some great data"}, tracking_item_id: tracking_item2.id,
+  from_trackable_system_id: ts_chemotion_electronic_laboratory_notebook.id, to_trackable_system_id: ts_radar4kit.id)
+Tracking.create!(status: :submitted, metadata: {data: "some awesome data"}, tracking_item_id: tracking_item4.id,
+  from_trackable_system_id: ts_nmrxiv.id, to_trackable_system_id: ts_radar4chem.id)
