56a42426fb16732d3ebde8be7775a7f44d36af9f
Christian Deme <christian.deme@cleanercode.de>
2023-01-31 16:03:28 +0100

    Move access to @params values to private methods

--- app/builders/tracking_builder.rb
+++ app/builders/tracking_builder.rb
@@ -19,20 +19,28 @@ class TrackingBuilder
 
   def metadata = @metadata ||= @params["metadata"]
 
+  def tracking_item_name = @tracking_item_name ||= @params["tracking_item_name"]
+
+  def from_trackable_system_name = @from_trackable_system_name ||= @params["from_trackable_system_name"]
+
+  def to_trackable_system_name = @to_trackable_system_name ||= @params["to_trackable_system_name"]
+
+  def tracking_item_owner_email = @tracking_item_owner_email ||= @params["tracking_item_owner_email"]
+
   def tracking_item = @tracking_item ||=
-                        TrackingItem.find_or_create_by!(name: @params["tracking_item_name"], user:)
+                        TrackingItem.find_or_create_by!(name: tracking_item_name, user:)
 
   def user
-    User.find_by(email: @params["tracking_item_owner_email"], role: :user) ||
-      User.create!(email: @params["tracking_item_owner_email"],
-        name: @params["tracking_item_owner_email"],
+    User.find_by(email: tracking_item_owner_email, role: :user) ||
+      User.create!(email: tracking_item_owner_email,
+        name: tracking_item_owner_email,
         role: :user,
         password: SecureRandom.base64(12))
   end
 
   def from_trackable_system = @from_trackable_system ||=
-                                TrackableSystem.find_by!(name: @params["from_trackable_system_name"])
+                                TrackableSystem.find_by!(name: from_trackable_system_name)
 
   def to_trackable_system = @to_trackable_system ||=
-                              TrackableSystem.find_by!(name: @params["to_trackable_system_name"])
+                              TrackableSystem.find_by!(name: to_trackable_system_name)
 end
