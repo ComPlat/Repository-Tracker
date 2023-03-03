0ebe3d546df91968116ca279a5a0b37ee1c72853
Christian Deme <christian.deme@cleanercode.de>
2023-03-03 14:33:46 +0100

    Force env client_id for Doorkeeper application factory

--- spec/factories/doorkeeper/doorkeeper_access_token.rb
+++ spec/factories/doorkeeper/doorkeeper_access_token.rb
@@ -2,7 +2,11 @@ FactoryBot.define do
   factory :doorkeeper_access_token, class: "Doorkeeper::AccessToken" do
     trait :with_required_dependencies do
       resource_owner_id { create(:user, :with_required_attributes_as_user).id }
-      application { create(:doorkeeper_application, :with_required_attributes) }
+      application {
+        create(:doorkeeper_application,
+          :with_required_attributes,
+          uid: ENV["DOORKEEPER_CLIENT_ID"])
+      }
     end
   end
 end
