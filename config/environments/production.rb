acffefec551366deb509263b06c4b9a8b7f8965a
Tobias Vetter <tobias.vetter@mailbox.org>
2023-03-08 15:07:48 +0100

    fixing bug with mailer from address not correctly set

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,7 +74,7 @@ Rails.application.configure do
     port: ENV["SMTP_PORT"],
     # domain: "yourdomain.com",
     authentication: ENV["SMTP_AUTHENTICATION"],
-    # enable_starttls_auto: true,
+    enable_starttls_auto: true,
     user_name: ENV["SMTP_USERNAME"],
     password: ENV["SMTP_PASSWORD"]
   }
--- run_docker_compose.production.sh
+++ run_docker_compose.production.sh
@@ -1,8 +1,9 @@
 SMTP_HOST=mail.smtp2go.com\
   SMTP_PORT=2525\
   SMTP_AUTHENTICATION=plain\
-  SMTP_USERNAME=mail\
-  SMTP_PASSWORD=mail\
+  SMTP_USERNAME=smtp_username\
+  SMTP_PASSWORD=smtp_password\
+  TRACKER_MAIL_SENDER=sender@replace-me.de\
   TRACKER_DB_DATA_FOLDER=./postgres\
   TRACKER_DB_USERNAME=postgres \
   TRACKER_DB_PASSWORD=postgres \
